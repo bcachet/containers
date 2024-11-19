eb0ac817216bc68dc213b6c9f2ede124882952ef
Bertrand Cachet <bertrand.cachet@gmail.com>
2024-11-19 10:13:15 +0100

    Go: debugger/lsp are nice to have

--- Containerfile.go
+++ Containerfile.go
@@ -5,3 +5,8 @@ RUN sudo dnf install -y \
         --nodocs --setopt install_weak_deps=False \
     && sudo dnf clean all -y
 
+RUN brew install golangci-lint
+
+RUN go install golang.org/x/tools/gopls@latest && \
+    go install github.com/go-delve/delve/cmd/dlv@master && \
+    go install go.uber.org/mock/mockgen@latest
